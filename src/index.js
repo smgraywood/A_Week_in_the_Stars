dd9ed9aad4af1a51d13f66567a72ded36cbd05eb
Sarah Graywood <smgraywood@gmail.com>
2023-05-13 10:26:08 -0700

    fix deploy issues

--- src/index.js
+++ src/index.js
@@ -7,9 +7,11 @@ import { BrowserRouter as Router } from "react-router-dom";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
+  <StrictMode>
 		<Router>
 			<App />
 		</Router>
+    </StrictMode>
 );
 
 // If you want to start measuring performance in your app, pass a function
