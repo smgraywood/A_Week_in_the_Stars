7b5e85239c0fdea3117aef0d3856c91562fc3243
Sarah Graywood <smgraywood@gmail.com>
2023-06-09 16:12:19 -0700

    heroku deploy

--- server/server.js
+++ server/server.js
@@ -2,7 +2,7 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const logger = require("morgan");
-require("dotenv").config({path: __dirname + '/.env'});
+// require("dotenv").config({path: __dirname + '/.env'});
 require("./config/database");
 
 //require index router module
