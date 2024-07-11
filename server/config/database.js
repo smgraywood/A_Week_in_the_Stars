f360d9831fb98289877d6edf2b32a02682379980
Sarah Graywood <smgraywood@gmail.com>
2024-07-10 22:07:36 -0700

    Fix database connection issues, enhance form styling, and sort database response

--- server/config/database.js
+++ server/config/database.js
@@ -1,7 +1,19 @@
 const mongoose = require("mongoose");
 
+
 //connect to db
-mongoose.connect(process.env.DATABASE_URL);
+mongoose.connect('mongodb://localhost:27017/guestbook', {
+	useNewUrlParser: true,
+	useUnifiedTopology: true,
+  }).then(() => {
+	console.log('Connected to MongoDB');
+  }).catch(err => {
+	console.error('Failed to connect to MongoDB', err);
+  });
+
+
+//connect to db
+// mongoose.connect(process.env.DATABASE_URL);
 
 //setup a shortcut var to the connection obj
 const db = mongoose.connection;
